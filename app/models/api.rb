8b0ecb2e74411783a22994dbc05fefd4e048203b
Connor Holland <supersoccergoalie123@gmail.com>
2021-03-31 11:54:03 -0600

    setup heroku

--- app/models/api.rb
+++ app/models/api.rb
@@ -2,5 +2,7 @@ require 'httparty'
 class Api
   def self.get_info(ticker)
     response = HTTParty.get("https://www.alphavantage.co/query?function=OVERVIEW&symbol=#{ticker}&apikey=#{ENV['API_KEY']}")
+    
+
   end
 end
